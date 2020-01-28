8cfafdd52fdf878ae9c202ca83275e2f8fde25b2
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-01-28 14:18:13 +0700

    refactor

--- products_e_commerce/spiders/products_spiders.py
+++ products_e_commerce/spiders/products_spiders.py
@@ -7,9 +7,9 @@ class ProductsSpider(scrapy.Spider):
     separator = 'n/'
 
     urls = [
-        'https://berrybenka.com/clothing/outwear/women/0',
-        'https://berrybenka.com/clothing/dresses/women/0',
-        'https://berrybenka.com/clothing/bottoms/women/0',
+        # 'https://berrybenka.com/clothing/outwear/women/0',
+        # 'https://berrybenka.com/clothing/dresses/women/0',
+        # 'https://berrybenka.com/clothing/bottoms/women/0',
         'https://berrybenka.com/clothing/tops/women/0'
     ]
 
@@ -25,22 +25,17 @@ class ProductsSpider(scrapy.Spider):
         products = response.css('#li-catalog')
 
         for product in products:
-            title = product.css('h1::text').get()
-            price = product.css('.discount::text').get()
-            image_source = product.css('.catalog-image').css('img::attr(src)').get()
-            page_url = product.css('a::attr(href)').get()
-            
-            items['title'] = title
-            items['price'] = price
-            items['image_source'] = image_source
-            items['page_url'] = page_url
+            items['title'] = product.css('h1::text').get()
+            items['price'] = product.css('.discount::text').get()
+            items['image_source'] = product.css('.catalog-image').css('img::attr(src)').get()
+            items['page_url'] =  product.css('a::attr(href)').get()
 
             yield items
         
         next_page = response.css('.right a::attr(href)').get()
         current_url = str(response.request.url)
         current_url = current_url.split(ProductsSpider.separator,1)
-
+        
         if next_page is not None:
             next_url = current_url[0] + ProductsSpider.separator + str(ProductsSpider.number_product)
             yield response.follow(next_url, callback = self.parse)
